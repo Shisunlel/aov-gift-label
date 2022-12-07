4f6e92f60e30b532e908cda80957bcad0a78e8cf
Rapol <vuthavyrapol@gmail.com>
2022-12-07 20:54:06 +0700

    Finish #5

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,28 @@
           <img src="/assets/santa.svg" alt="" class="max-w-[12em]" />
           <div>
             <h1 class="font-serif text-4xl">Happy Holidays!</h1>
-            <div class="mt-4 bg-white text-black p-2 rounded">TO: John</div>
-            <div class="mt-2 bg-white text-black p-2 rounded">FROM: Joana</div>
+            <div class="mt-4 bg-white text-black p-2 rounded">TO: {{ message.to }}</div>
+            <div class="mt-2 bg-white text-black p-2 rounded">FROM: {{ message.from }}</div>
           </div>
         </div>
+      </div>
+      <div class="flex flex-col py-4">
+        <label for="To">To:</label>
+        <input class="w-full rounded shadow p-2 border" type="text" id="To" v-model="message.to">
+      </div>
+      <div class="flex flex-col py-4">
+        <label for="From">From:</label>
+        <input class="w-full rounded shadow p-2 border" type="text" id="From" v-model="message.from">
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-// ...
+import { reactive } from 'vue';
+
+const message = reactive({
+  to: '',
+  from: ''
+})
 </script>
